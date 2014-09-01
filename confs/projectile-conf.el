afb4bed71aaa89823b1f13f830e9c66bda566f95
labinxu <flowinair@gmail.com>
2014-09-01 18:21:23 +0800

    fixed

--- confs/projectile-conf.el
+++ confs/projectile-conf.el
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 (require 'projectile)
 (projectile-global-mode)
 (setq projectile-enable-caching t)
-=======
-(require 'projectile)
-(projectile-global-mode)
->>>>>>> 1b79cf4e95837f19f7e6ef74989e6ef37b318383
+
