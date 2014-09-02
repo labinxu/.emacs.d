97f38012814c580e75bb2df0ecf1d65e8e5e9306
labinxu <flowinair@gmail.com>
2014-09-02 18:10:42 +0800

    ad

--- confs/tabbar-conf.el
+++ confs/tabbar-conf.el
@@ -1,26 +1,2 @@
 (require 'tabbar)
 (tabbar-mode 1)
-(setq tabbar-buffer-list-function
-    (lambda ()
-        (remove-if
-          (lambda(buffer)
-             (find (aref (buffer-name buffer) 0) " *"))
-          (buffer-list))))
-(setq tabbar-buffer-groups-function
-      (lambda()(list "All")))
-(set-face-attribute 'tabbar-button nil)
-
-;;set tabbar's backgroud color
-(set-face-attribute 'tabbar-default nil
-                    :background "gray"
-                    :foreground "gray30")
-(set-face-attribute 'tabbar-selected nil
-                    :inherit 'tabbar-default
-                    :background "green"
-                    :box '(:line-width 3 :color "DarkGoldenrod") )
-(set-face-attribute 'tabbar-unselected nil
-                    :inherit 'tabbar-default
-                    :box '(:line-width 3 :color "gray"))
-
-;; USEFUL: set tabbar's separator gap
-(custom-set-variables '(tabbar-separator (quote (1.5))))
