c7eeab454799638c246d3da8ca8334a7264a4e71
labinxu <flowinair@gmail.com>
2014-09-11 10:48:31 +0800

    smartcompile

--- confs/cpp-conf.el
+++ confs/cpp-conf.el
@@ -1,2 +1,37 @@
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
+
+
+(global-set-key (kbd "<f7>") 'smart-compile) 
+(defun smart-compile() 
+  (interactive) 
+  (let ((candidate-make-file-name '("makefile" "Makefile" "GNUmakefile")) 
+        (command nil)) 
+    (if (not (null 
+              (find t candidate-make-file-name :key 
+                    '(lambda (f) (file-readable-p f))))) 
+        (setq command "make -k ") 
+        ;; makefile not found, check the current mode
+      (if (null (buffer-file-name (current-buffer))) 
+          (message "Buffer not attached to a file, won't compile!") 
+        (if (eq major-mode 'c-mode) 
+            (setq command 
+                   ;;make sure where the compiler 
+                  (concat "gcc -Wall -o "
+                          (file-name-sans-extension
+                           (file-name-nondirectory buffer-file-name))
+                          " " 
+                          (file-name-nondirectory buffer-file-name) 
+                          " -g -lm ")) 
+          (if (eq major-mode 'c++-mode) 
+              (setq command 
+                    (concat "g++ -Wall -o "
+                            (file-name-sans-extension 
+                             (file-name-nondirectory buffer-file-name)) 
+                            " " 
+                            (file-name-nondirectory buffer-file-name) 
+                            " -g -lm ")) 
+            (message "Unknow mode, won't compile!"))))) 
+    (if (not (null command)) 
+        (let ((command (read-from-minibuffer "Compile command: " command))) 
+          (compile command)))))
