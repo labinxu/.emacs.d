2635b8b13f7ca6f6f74c56820358e2fc41542dd4
Laibin Xu <ext-laibin.xu@microsoft.com>
2014-09-25 15:35:52 +0800

    grizzal

--- confs/projectile-conf.el
+++ confs/projectile-conf.el
@@ -2,3 +2,7 @@
 (projectile-global-mode)
 (setq projectile-enable-caching t)
 
+(require 'grizzl)
+(setq projectile-completion-system 'grizzl)
+(global-set-key (kbd "s-p") 'projectile-find-file)
+(global-set-key (kbd "s-b") 'projectile-switch-to-buffer)
--- confs/tabbar-conf.el
+++ confs/tabbar-conf.el
@@ -1,8 +1,8 @@
 (require 'tabbar)
 (tabbar-mode 1)
-(custom-set-variables '(tabbar-separator (quote (0.5))))
-(global-set-key [(C tab)] 'tabbar-forward)
-(global-set-key [(C S tab)] 'tabbar-backward)
+(custom-set-variables '(tabbar-separator (quote (1))))
+(global-set-key [(C tab)] 'tabbar-forward-tab)
+(global-set-key [(C S tab)] 'tabbar-backward-tab)
 
 
 ;;(setq tabbar-buffer-groups-function
@@ -20,3 +20,26 @@
 (set-face-attribute 'tabbar-unselected nil
                     :inherit 'tabbar-default
                     :box '(:line-width 3 :color "gray"))
+(defun my-tabbar-buffer-groups ()  
+  "Return the list of group names the current buffer belongs to.  
+Return a list of one element based on major mode."  
+  (list  
+   (cond  
+    ((or (get-buffer-process (current-buffer))  
+         ;; Check if the major mode derives from `comint-mode' or  
+         ;; `compilation-mode'.  
+         (tabbar-buffer-mode-derived-p  
+          major-mode '(comint-mode compilation-mode)))  
+     "Process"  
+     )  
+    ((string-equal "*" (substring (buffer-name) 0 1))  
+     "Emacs Buffer"  
+     )  
+    ((eq major-mode 'dired-mode)  
+     "Dired"  
+     )  
+    (t  
+     "User Buffer"  
+     ))))  
+  
+(setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)  
