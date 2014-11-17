4f7f98b7bb4144a71f089aea12b576dc38b8160d
labinxu <flowinair@gmail.com>
2014-11-17 16:35:22 +0800

    highlight

---
+++ confs/highlight-conf.el
@@ -0,0 +1,20 @@
+;; -*- Emacs-Lisp -*-
+
+;; Time-stamp: <2010-04-10 22:38:44 Saturday by ahei>
+
+(require 'highlight-parentheses)
+(add-hook 'c++-mode-hook 'highlight-parentheses-mode)
+(add-hook 'c-mode-hook 'highlight-parentheses-mode)
+(add-hook 'python-mode-hook 'highlight-parentheses-mode)
+
+(require 'highlight-indentation)
+(add-hook 'c++-mode-hook 'highlight-indentation-mode)
+(add-hook 'c-mode-hook 'highlight-parentheses-mode)
+(add-hook 'python-mode-hook 'highlight-parentheses-mode)
+
+
+(require 'highlight-symbol)
+(global-set-key [(control f4)] 'highlight-symbol-at-point)
+(global-set-key [f4] 'highlight-symbol-next)
+(global-set-key [(shift f4)] 'highlight-symbol-prev)
+(global-set-key [(meta f4)] 'highlight-symbol-query-replace)
